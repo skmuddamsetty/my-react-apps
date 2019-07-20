1c01420755cadc9f7a24fa3c4b5f492dc0457a98
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2019-07-20 15:08:29 -0400

    adjusted styling of the car comparator page and added car comparator page

--- src/components/car-comparator/CarComparatorPage.js
+++ src/components/car-comparator/CarComparatorPage.js
@@ -9,32 +9,8 @@ const CarComparatorPage = () => (
             <option>One</option>
             <option>Two</option>
           </select>
-          <select className="car-comparator__container-main-box-1-select">
-            <option>One</option>
-            <option>Two</option>
-          </select>
-          <select className="car-comparator__container-main-box-1-select">
-            <option>One</option>
-            <option>Two</option>
-          </select>
-          <select className="car-comparator__container-main-box-1-select">
-            <option>One</option>
-            <option>Two</option>
-          </select>
         </div>
         <div className="car-comparator__container-main-box-2">
-          <select className="car-comparator__container-main-box-2-select">
-            <option>One</option>
-            <option>Two</option>
-          </select>
-          <select className="car-comparator__container-main-box-2-select">
-            <option>One</option>
-            <option>Two</option>
-          </select>
-          <select className="car-comparator__container-main-box-2-select">
-            <option>One</option>
-            <option>Two</option>
-          </select>
           <select className="car-comparator__container-main-box-2-select">
             <option>One</option>
             <option>Two</option>
